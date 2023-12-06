80062bb4355972ac84e807120c0e44e716f94d00
inahuel98 <iquispe@fi.uba.ar>
2023-12-06 13:01:52 -0300

    desaf json creados

--- desafio-json/desa1/datosBici.js
+++ desafio-json/desa1/datosBici.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
-let bicis = fs.readFileSync("./bicicletas.json", "utf-8");
+const bicis = fs.readFileSync("bicicletas.json", "utf-8");
 
-let bicisArray = JSON.parse(bicis);
+const bicisArray = JSON.parse(bicis);
+
+
 console.log(bicisArray);
+module.exports = bicisArray;
---
+++ desafio-json/desa2/app.js
@@ -0,0 +1,2 @@
+const datosBicicleta = require("../desa1/datosBici.js");
+console.log(datosBicicleta);
